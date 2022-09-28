288d223ecfe219d51ee7aa9219c6bb9d5a6f65e0
Mikhail Malafeew <malafeew2016@gmail.com>
2022-09-28 21:32:42 +0300

    C4

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void PrintArray(int[] arr)
+            {
+                for (int i = 0; i < arr.Length; i++)
+			    {
+                    Console.Write(arr[i] + " ");
+			    }
+            }
+            void SortArray(int[] arr)
+            {
+                int index = 0;
+                int tmp = 0;
+                for (int i = 0; i < arr.Length; i++)
+			    {
+                   index = i;
+                   while (index>0 && arr[index-1] > arr[index])
+                   {
+                        tmp = arr[index-1];
+                        arr[index-1] = arr[index];
+                        arr[index] = tmp;
+                        index--;
+                   }
+        
+			    }
+            }
+            int[] array = { 2, 3, 1, 5, 6, 1 };
+            PrintArray(array);
+            SortArray(array);
+            Console.WriteLine("\nПосле сортировки:");
+            PrintArray(array);
+            Console.ReadLine();
+        }
+
+    }
+}
